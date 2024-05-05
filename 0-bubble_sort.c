b8a319d1df39fa5b0615168fc3c3826449018e74
Wiraama <mawira01@students.uonbi.ac.ke>
2024-05-05 06:45:28 -0700

    quiz 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,25 @@
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, high;
+	int temp;
+
+	high = size - 1;
+	/**loop to alterate through the array to end**/
+	for (i = 0; i < high; i++)
+	{
+		/**loop to check what stored and arrange them**/
+		for (j = 0; j <high - i; j++)
+		{
+			if (array[j] < array[j + 1])
+			{
+				/*swapping*/
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
