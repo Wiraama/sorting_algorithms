b5547d30b597487a56047548df434e6878ba4d79
Wiraama <mawira01@students.uonbi.ac.ke>
2024-05-05 18:04:37 -0700

    all

--- sort.h
+++ sort.h
@@ -30,4 +30,14 @@ void swap_ints(int *a, int *b);
 int lomuto_partition(int *array, size_t size, int left, int right);
 void lomuto_sort(int *array, size_t size, int left, int right);
 void quick_sort(int *array, size_t size);
+int _strcmp(const char *s1, const char *s2);
+char get_value(deck_node_t *card);
+void insertion_sort_deck_kind(deck_node_t **deck);
+void insertion_sort_deck_value(deck_node_t **deck);
+void sort_deck(deck_node_t **deck);
+void swap_ints(int *a, int *b);
+void shell_sort(int *array, size_t size);
+void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
+void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
+void cocktail_sort_list(listint_t **list);
 #endif /*SORT_h*/
